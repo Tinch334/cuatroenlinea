991a37ae692180b3938e18bfc9df3b5e434776a2
Martin Goñi <screadores122@gmail.com>
2022-08-25 18:07:15 -0300

    Code cleanup in DetectWin
    Cleaned the code for detect win class, removing duplicate code

--- app/DetectWin.php
+++ app/DetectWin.php
@@ -46,32 +46,15 @@ class DetectWin implements DetectWinInterface {
         //The min check is done to prevent going outside of the boards bounds.
         //The "break" is in place to avoid a situation where we count disconnected pieces. For example without the break, the line "BBRB" would count 3 pieces, when there are only to connected.
         for ($xCount = $x; $xCount < min($x + 3, $this->xSize); $xCount++) { 
-            $checkPiece = $this->board->getSpace($xCount, $y);
-
-            //Make sure we are not checking an empty space.
-            if ($checkPiece == NULL)
-                break;
-
-            //We check if the current piece has the same colour as the base piece.
-            if ($checkPiece->getColourInt() == $pieceColour)
+            if ($this->pieceCheck($this->board->getSpace($xCount, $y), $pieceColour))
                 $matchCount++;
             else
                 break;
         }
 
-        if ($matchCount >= $winCount)
-            return true;
-
         //If we didn't find a match to the right we try to the left.
         for ($xCount = $x; $xCount > max($x - 3, 0); $xCount--) { 
-            $checkPiece = $this->board->getSpace($xCount, $y);
-
-            //Make sure we are not checking an empty space.
-            if ($checkPiece == NULL)
-                break;
-
-            //We check if the current piece has the same colour as the base piece.
-            if ($checkPiece->getColourInt() == $pieceColour)
+            if ($this->pieceCheck($this->board->getSpace($xCount, $y), $pieceColour))
                 $matchCount++;
             else
                 break;
@@ -85,32 +68,15 @@ class DetectWin implements DetectWinInterface {
         $matchCount = 0; //Reset match count.
 
         for ($yCount = $y; $yCount < min($y + 3, $this->ySize); $yCount++) { 
-            $checkPiece = $this->board->getSpace($x, $yCount);
-
-            //Make sure we are not checking an empty space.
-            if ($checkPiece == NULL)
-                break;
-
-            //We check if the current piece has the same colour as the base piece.
-            if ($checkPiece->getColourInt() == $pieceColour)
+            if ($this->pieceCheck($this->board->getSpace($x, $yCount), $pieceColour))
                 $matchCount++;
             else
                 break;
         }
-
-        if ($matchCount >= $winCount)
-            return true;
         
         //If we didn't find a match to the right we try to the left.
         for ($yCount = $y; $yCount > max($y - 3, 0); $yCount--) { 
-            $checkPiece = $this->board->getSpace($x, $yCount);
-
-            //Make sure we are not checking an empty space.
-            if ($checkPiece == NULL)
-                break;
-
-            //We check if the current piece has the same colour as the base piece.
-            if ($checkPiece->getColourInt() == $pieceColour)
+            if ($this->pieceCheck($this->board->getSpace($x, $yCount), $pieceColour))
                 $matchCount++;
             else
                 break;
@@ -120,6 +86,19 @@ class DetectWin implements DetectWinInterface {
             return true;
 
         return false;
+    }
+
+    //Takes a piece and a colour and checks if the colour of the piece matches the colour. Note, the "?Piece" means that the argument can either be an object or "NULL".
+    protected function pieceCheck(?Piece $pieceToCheck, int $pieceColour): bool {
+        //Make sure we are not checking an empty space.
+        if ($pieceToCheck == NULL)
+            return false;
+
+        //We check if the current piece has the same colour as the base piece.
+        if ($pieceToCheck->getColourInt() == $pieceColour)
+            return true;
+        else
+            return false;
     }
 }
 
